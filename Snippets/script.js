22a413d24ac4d109298d97e2fbaad9d6a1f1a2a8
c00villamj <marvilla12@publicisgroupe.net>
2024-09-23 12:32:43 -0300

    Agrega archivos de configuración y código fuente para proyecto de migración

---
+++ Snippets/script.js
@@ -0,0 +1,21 @@
+console.log('Hola mundo');
+console.log('Como estas?');
+console.log('Este es mi primer snippet');
+console.log('creado en Visual Studio Code');
+console.log('que les parece?');
+
+console.log('como estas?');
+console.log('gracias por preguntar');
+console.log('yo estoy bien');
+console.log('me alegra que estes bien');
+console.log('Hola mundo');
+
+ConvolverNode.addEventListener('utf-8', function() {
+    console.log('	gracias por preguntar');
+});
+
+console.log('asda');
+console.log('hola como estan?');
+console.log('sdas');
+console.log('asda');
+console.log('hola como estan?');
---
+++ Snippets/snippet.java
@@ -0,0 +1,10 @@
+package Snippets;
+
+public class snippet {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+        // code
+        }
+        System.out.println("Hello World");
+    }
+}
