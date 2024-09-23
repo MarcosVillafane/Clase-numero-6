377b29e1384046b8cdced77ed08572379d3606ab
c00villamj <marvilla12@publicisgroupe.net>
2024-09-23 10:32:19 -0300

    Agrega archivos de configuración y código fuente para proyecto de migración

---
+++ PruebaDeRunner/prueba.java
@@ -0,0 +1,7 @@
+package PruebaDeRunner;
+
+public class prueba {
+	public static void main(String[] args) {
+		System.out.println("Hola a todos");
+	}
+}
---
+++ proyectoEclipse/ProyectoPruebaMigracion/src/migra/Migra.java
@@ -0,0 +1,9 @@
+package proyectoEclipse.ProyectoPruebaMigracion.src.migra;
+
+public class Migra {
+
+	public static void main(String[] args) {
+		System.out.println("Hola a todos");
+		System.out.println("Esta es mi pequeña prueba de migracion");
+	}
+}
